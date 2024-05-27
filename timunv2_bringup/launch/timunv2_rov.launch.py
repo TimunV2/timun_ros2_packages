f80594257db360ba520b735ae158f92fe905b942
bangsan19 <hasandzulfadli@gmail.com>
2024-05-27 08:30:11 +0700

    modify launch

---
+++ timunv2_bringup/launch/timunv2_rov.launch.py
@@ -0,0 +1,34 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    joy_node = Node(
+        package="joy",
+        executable="joy_node"
+    )
+
+    joy_sub_node = Node(
+        package="timunv2_controller",
+        executable="joy_sub_node"
+    )
+
+    master_controller_node = Node(
+        package="timunv2_controller",
+        executable="master_controller"
+    )
+
+    serial_node = Node(
+        package="timunv2_serial",
+        executable="serial_node"
+    )
+
+    # ld.add_action(joy_node) #to read pilot input via joy stick like button and analog sticks
+    # ld.add_action(joy_sub_node) #to convert button and axis to velocity and utility command
+    ld.add_action(master_controller_node) #to select the final output of cmd velocity and set point
+    ld.add_action(serial_node) #to convert and send the data to stm, and to read sensor data
+
+    return ld
